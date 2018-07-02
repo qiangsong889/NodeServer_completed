9d9ee562c66b97f8ed83dea78043804e95757a58
qiang song <qiangsong889@yahoo.com>
2018-07-02 16:10:29 -0700

    tutorial on react and webpack 4

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <div>hello world!</div>;
+  }
+}
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //library interact with dom element
+import App from './App.jsx'; //components
+ReactDOM.render(<App />, document.getElementById('app'));
