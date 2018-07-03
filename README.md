226e13f3152a8f94a90c5b3d42734340ebe7c3c2
qiang song <qiangsong889@yahoo.com>
2018-07-02 21:24:22 -0700

    NodeJS and express

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # webpack-4-react
-tutorial: basic react and webpack 4 setups
 
-please check out this article 
-https://medium.com/@359529995/easy-set-up-on-webpack-4-with-react-9eb8ba3be19f
+tutorial: Node server set up with react
+
+open terminal do
+-npm install
+-npm run dev
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      number: 0,
+      double: undefined
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+  sendNumber() {
+    console.log('ha', this.state.number);
+    axios
+      .post('/doubleNumber', {
+        number: this.state.number
+      })
+      .then(response => {
+        this.setState({
+          double: response.data
+        });
+      })
+      .catch(err => {
+        console.log('got an error doing axios request', err);
+      });
+  }
+  changeState(number) {
+    console.log('you want to change the state', number);
   }
   render() {
-    return <div>hello world!</div>;
+    return (
+      <div>
+        enter a number, server will double it and send it back
+        <br />
+        <input
+          type="text"
+          onChange={e => this.setState({ number: e.target.value })}
+        />
+        <button onClick={() => this.sendNumber()}>submit</button>
+        <h1>{this.state.double}</h1>
+      </div>
+    );
   }
 }
 export default App;
